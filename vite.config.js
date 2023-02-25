5b27f3fcade3f99f03fd7d18d21b5ccc803aa237
ditrix <ditrix2006@gmail.com>
2023-02-25 11:49:22 +0200

    layouts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import  vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -11,7 +12,7 @@ export default defineConfig({
         vue({
             template: {
                 base: null,
-                includeAbsolute: false
+                includeAbsolute: false,
             }
         })
     ],
